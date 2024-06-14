77ea124e985eb5ab06fb423fa7fa768a24a7695a
Saidkodirov <stolibjon123@gmail.com>
2024-06-14 15:33:40 +0500

    m

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6c@*+j#!cg^ihl$960w6w@=@b7*#v)2kw7v=#_(zj68!!r-9*=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "127.0.0.1:8000", ".pythonanywhere.com"]
 
 
 # Application definition
