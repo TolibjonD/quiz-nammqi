64bd49c23347190112f005df898cea21f3890bf9
Saidkodirov <stolibjon123@gmail.com>
2024-06-14 15:31:32 +0500

    False

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6c@*+j#!cg^ihl$960w6w@=@b7*#v)2kw7v=#_(zj68!!r-9*='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
